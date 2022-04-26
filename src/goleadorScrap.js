617ba1e19d229f6294ab70efd9c6b619bd59ebd7
ThiagoDes <thiagothic25@gmail.com>
2022-04-25 21:15:20 -0300

    iniciando projeto

---
+++ src/goleadorScrap.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const url = 'https://ge.globo.com/pe/futebol/campeonato-pernambucano/';
+
+axios(url).then(response => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const tabelaStatus = $('.ranking-item-wrapper');
+    const tabelaJogador = [];
+    tabelaStatus.each(function(){
+        const nomeJogador = $(this).find('.jogador-nome').text();
+        const posicaoJogador = $(this).find('.jogador-posicao').text();
+        const numeroGols = $(this).find('.jogador-gols').text();
+        const timeJogador = $(this).find('.jogador-escudo > img').attr('alt');
+        tabelaJogador.push({
+            nomeJogador,
+            posicaoJogador,
+            numeroGols,
+            timeJogador
+        });
+    });
+    console.log(tabelaJogador);
+}).catch(console.error);
